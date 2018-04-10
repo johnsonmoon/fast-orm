2243ef3602221517c36b4ab67c97a35a3320adbd
xuyh <xuyh@uyunsoft.cn>
2018-04-10 18:06:27 +0800

    Package info.

---
+++ core/src/main/java/com/github/johnsonmoon/fastorm/core/annotation/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * this package stores annotation type used for annotate database object class.
+ *
+ * Created by johnsonmoon at 2018/4/10 17:48.
+ */
+package com.github.johnsonmoon.fastorm.core.annotation;
